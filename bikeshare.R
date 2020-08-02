ce0d9dbe0e0a8850c36cf14d2683d243313fe168
laurencehorton <56881397+laurencehorton@users.noreply.github.com>
2020-08-02 19:08:00 -0400

    Update bikeshare.R

--- bikeshare.R
+++ bikeshare.R
@@ -17,16 +17,24 @@ station <- as.data.frame(bikestation[["data"]][["stations"]])
 bike <- merge(station, status, by="station_id")
 
 # Keep only the stations close to me
-mybike <- subset(bike, station_id %in% c("7071", "7042", "7549"))
-# Keep only name of station and number of bikes available
-available <- subset(mybike, select = c("name", "num_bikes_available"))
+# Create geobox boundaries
+n <- (43.66731) # north
+s <- (43.66312) # south
+e <- (-79.3742) # east
+w <- (-79.3696) # west
+
+mybike <- bike%>% select("name", "lat", "lon", "num_bikes_available") %>% filter(lat < n & lat > s & lon > e & lon < w)# Keep only name of station and number of bikes available
+mybike <- subset(mybike, select = -c(lat,lon) ) # Drop the geocordinates
+colnames(mybike)[1] <- "Station"
+colnames(mybike)[2] <- "Available"
 print(available)
 
 # remove items no longer needed
-rm(bike, bikestation, bikestatus, mybike, station, status)
+rm(bike, bikestation, bikestatus, n, s, e, w, station, status)
 
 # Citations----
 citation()
+citation("dplyr")
 citation("jsonlite")
 sessionInfo()
 
